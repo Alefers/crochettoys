67b3858378630c99861ca57d7991958f817e1fce
Alefers <alefersxaoc@gmail.com>
2017-09-23 14:06:45 +0300

    just commit

--- contacts.php
+++ contacts.php
@@ -2,27 +2,23 @@
 $page = 'contacts';
 include 'scripts/header.php';
 ?>
-<div id="context">
-   <div id="cont_box">
-      <img src="design/cont.jpg" alt="Вероника Буеверова">
-      <div style="height: 1px;"></div>
-      <div id="cont_txt">
-         <h3>Veronika</h3>
-         <p>
+      <main>
+        <img src="design/cont.jpg" alt="Вероника Буеверова">
+        <div class="cont_txt">
+          <h1>Veronika</h1>
+          <p>
             tel: <a href="tel:+37125534036">+371 25534036</a>
-         </p>
-         <p>
+          </p>
+          <p>
             mail: <a href="mailto:bueverova@gmail.com">bueverova@gmail.com</a>
-         </p>
-         <p>
-            instagramm: <a href="https://www.instagram.com/crochettoys_lv/">crochettoys_lv</a>
-         </p>
-         <p>
-            FB: <a href="https://www.facebook.com/crochettoyslv/">crochettoyslv</a>
-         </p>
-      </div>
-   </div>
-   <div id="before_footer"></div>
-</div>
+          </p>
+          <p>
+            instagramm: <a href="https://www.instagram.com/crochettoys_lv/" target="_blank">crochettoys_lv</a>
+          </p>
+          <p>
+            FB: <a href="https://www.facebook.com/crochettoyslv/" target="_blank">crochettoyslv</a>
+          </p>
+        </div>
+      </main>
 <?php
 include 'scripts/footer.php';
